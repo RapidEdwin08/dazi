82d36c9d2bacbab6f46c7d63928a0fe3c1c5130c
Rapid Edwin <88399538+RapidEdwin08@users.noreply.github.com>
2022-07-30 17:29:42 -0400

    v2022.08
    Added -warp -skill

--- DAZI-Template.sh
+++ DAZI-Template.sh
@@ -1,13 +1,18 @@
-# https://github.com/RapidEdwin08/dazi  #v2022.07
+# https://github.com/RapidEdwin08/dazi  #v2022.08
 
 # Define [D00M.WAD] - /full/path/to/doom.wad
 doomWAD=~/RetroPie/roms/ports/doom/doomu.wad
 
-# Define As Many [doomMOD#s] As Needed Starting with #1 - Ascending Numeric 0rder
+# 0ptional Define As Many [doomMOD#s] As Needed Starting with #1 - Ascending Numeric 0rder
 doomMOD1=~/RetroPie/roms/ports/doom/mods/SIGIL.zip
 doomMOD2=~/RetroPie/roms/ports/doom/mods/Modifiers/WeaponsOfSaturn/weapons_of_saturn.pk3
 doomMOD3=~/RetroPie/roms/ports/doom/mods/HUDs/WOSHUD.pk3
 doomMOD4=~/RetroPie/roms/ports/doom/mods/0ther/DooM_-_Double_Jump.pk3
+
+# 0ptional WARP and DIFFICULTY Settings For [lzdoom-dazi+warp] - eg. Ultimate Doom SIGIL E5M1 on Ultra-Violence - episodeNUM=5 mapNUM=1 skillLEVEL=4
+episodeNUM=
+mapNUM=
+skillLEVEL=
 
 # Define addonDIR - dazi TMPFS - Default RetroPie ADDON
 addonDIR=/dev/shm/addon
@@ -22,6 +27,12 @@ count=$(( $count + 1 )) # Increase count by+1
 rollingM0D="doomMOD${count}" # Apply Increased count to [doomMOD#]
 if [[ ! "${!rollingM0D}" == "" ]]; then addonDIRcount=$(( $addonDIRcount + 1 )) && loadM0Ds; fi # Prepare more [doomMOD#s] IF DEFINED
 }
+
+# Prepare WARP and DIFFICULTY Settings For [lzdoom-dazi+warp]
+if [ ! "$episodeNUM" == "" ] || [ ! "$mapNUM" == "" ]; then # 0nly Update [-warp*] in [emulators.cfg] IF the Parameters are DEFINED
+	if [ "$skillLEVEL" == "" ]; then skillLEVEL=3; fi # Set DEFAULT DIFFICULTY [Hurt me plenty] if NOT DEFINED
+	sed -i "s/-warp.*/-warp\ $episodeNUM\ $mapNUM\ -skill\ $skillLEVEL\"/g" /opt/retropie/configs/ports/doom/emulators.cfg #Update [-warp*] in [emulators.cfg]
+fi
 
 mkdir "$addonDIR" > /dev/null 2>&1 # Prepare addonDIR
 count=1 # Set Initial Count
